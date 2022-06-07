608a3303a7f740532ec07964c7b46553a89d868f
Yelyzaveta_Stupnikova <71174667+qwerty474@users.noreply.github.com>
2022-06-08 01:20:58 +0200

    Update AbstractPage.java

--- src/main/java/abstractclasses/page/AbstractPage.java
+++ src/main/java/abstractclasses/page/AbstractPage.java
@@ -1,9 +1,23 @@
 package abstractclasses.page;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 public abstract class AbstractPage {
 
     private String pageUrl;
     private String pageUrlPattern;
+    private String pageTitle;
+
+    public void setTitle(String title) {
+        this.pageTitle = title;
+    }
+
+    public String getTitle() {
+        if (pageTitle.isEmpty()) {
+            return EMPTY;
+        }
+        return pageTitle;
+    }
 
     public void setPageUrl(String pageUrl) {
         this.pageUrl = pageUrl;
